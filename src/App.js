67e3168ff44922d500a76d0c10afd42b5e397f74
shallihan <smhallihan@gmail.com>
2021-05-27 10:28:41 +0100

    created form component, no styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ExpenseForm from './components/ExpenseForm/ExpenseForm';
 import './App.css';
 
 
 function App() {
+  const [categoryInput, setCategoryInput] = useState('');
+  const [expenseInput, setExpenseInput] = useState('');
+  const [costInput, setCostInput] = useState('');
+  const [expenses, setExpenses] = useState([]);
+
   return (
     <div className="App">
      <div className="container">
-       
+     <ExpenseForm 
+       categoryInput={categoryInput}
+       setCategoryInput={setCategoryInput}
+       expenseInput={expenseInput}
+       setExpenseInput={setExpenseInput} 
+       costInput={costInput}
+       setCostInput={setCostInput}
+       expenses={expenses}
+       setExpenses={setExpenses}
+      />
      </div>
     </div>
   );
---
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+
+const ExpenseForm = 
+  ({
+      categoryInput, 
+      setCategoryInput, 
+      expenseInput, 
+      costInput, 
+      setExpenseInput, 
+      setCostInput, 
+      expenses, 
+      setExpenses,
+    }) => {
+
+    const inputCategoryHandler = (e) => {
+        setCategoryInput(e.target.value);
+    };
+
+    const inputExpenseHandler = (e) => {
+        setExpenseInput(e.target.value);
+    };
+
+    const inputCostHandler = (e) => {
+        setCostInput(e.target.value);
+    };
+    
+    const submitExpenseHandler = (e) => {
+        e.preventDefault();
+        setExpenses([
+            ...expenses, 
+            {id: uuidv4(), category: categoryInput, item: expenseInput, cost: costInput, timestamp: Date.now()},
+        ]);
+        setCategoryInput('');
+        setExpenseInput('');
+        setCostInput('');
+    }
+
+    return (
+        <form>
+            <div className="form-inputs">
+              <input 
+                type="text" 
+                className="category" 
+                placeholder="eg. Home & Utilities" 
+                value={categoryInput} 
+                onChange={inputCategoryHandler} 
+                maxLength="25"
+                required="required"/>
+           <input 
+                type="text" 
+                className="expense" 
+                placeholder="eg. Drinks" 
+                value={expenseInput} 
+                onChange={inputExpenseHandler} 
+                maxLength="25"
+                required="required"/>
+              <div className="cost">
+                <label htmlFor="cost">Amount Spent</label>
+                <input 
+                  type="text" 
+                  name="cost" 
+                  id="cost" 
+                  placeholder="£" 
+                  value={costInput} 
+                  onChange={inputCostHandler}
+                  maxLength="8" 
+                  required="required"/>
+              </div>
+            </div>
+            <button type="submit" className="submit-button" onClick={submitExpenseHandler}><i className="fas fa-plus"></i></button>
+        </form>
+    );
+}
+
+
+export default ExpenseForm;
