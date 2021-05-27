119c3b84682df3187aba6315b169df3ef405caac
shallihan <smhallihan@gmail.com>
2021-05-27 10:18:35 +0100

    creating components folder/file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      Hello
+     <div className="container">
+       
+     </div>
     </div>
   );
 }
