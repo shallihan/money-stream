2c400b50309f4a150d84486889675b6a24ad331a
shallihan <smhallihan@gmail.com>
2021-05-27 10:39:54 +0100

    created chart component using recharts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import ExpenseForm from './components/ExpenseForm/ExpenseForm';
 import ExpenseList from './components/ExpenseList/ExpenseList';
+import ExpenseChart from './components/ExpenseChart/ExpenseChart';
 import './App.css';
 
 
@@ -11,9 +12,37 @@ function App() {
   const [costInput, setCostInput] = useState('');
   const [expenses, setExpenses] = useState([]);
 
+  const home = "Home & Utilities";
+  const travel =  "Travel";
+  const food = "Food & Groceries";
+  const media = "Media";
+  const bars = "Bars & Restaurants";
+
+  const getTotalCategory = (category) => {
+    if(expenses === null) {
+      return null;
+    } else {
+    let filteredCategories = expenses.filter((element) => element.category === category);
+    let allCosts = filteredCategories.map(function(element) { return parseFloat(element.cost) });
+    return allCosts;
+    }
+  }
+
+  let totalHome = getTotalCategory(home);
+  let totalTravel = getTotalCategory(travel);
+  let totalFood = getTotalCategory(food);
+  let totalMedia = getTotalCategory(media);
+  let totalBars = getTotalCategory(bars);
+
   return (
     <div className="App">
      <div className="container">
+     <ExpenseChart 
+       totalHome={totalHome}
+       totalTravel={totalTravel}
+       totalFood={totalFood}
+       totalMedia={totalMedia}
+       totalBars={totalBars}/>
      <ExpenseForm 
        categoryInput={categoryInput}
        setCategoryInput={setCategoryInput}
---
+++ src/components/ExpenseChart/ExpenseChart.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { PieChart, Pie, Cell} from 'recharts';
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#6600FF" ];
+
+
+const ExpenseChart = ({totalHome, totalTravel, totalFood, totalMedia, totalBars}) => {
+
+const sumTotals = (totals) => {
+    if(totals.length > 0) {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return totals.reduce(reducer);
+    } else {
+      return 0;
+    }
+  }
+       let sumHome = sumTotals(totalHome);
+       let sumTravel = sumTotals(totalTravel);
+       let sumFood = sumTotals(totalFood);
+       let sumMedia = sumTotals(totalMedia);
+       let sumBars = sumTotals(totalBars);
+
+      const data = [
+        { name: 'Home', value: sumHome },
+        { name: 'Travel', value: sumTravel},
+        { name: 'Food', value: sumFood },
+        { name: 'Media', value: sumMedia },
+        { name: 'Bars', value: sumBars},
+      ];
+
+    return (
+    <PieChart width={350} height={350}>
+        <Pie
+          data={data}
+          cx={200}
+          cy={200}
+          innerRadius={80}
+          outerRadius={120}
+          fill="#8884d8"
+          paddingAngle={5}
+          dataKey="value"
+        >
+         {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+        </Pie>
+      </PieChart>
+    );
+}
+
+export default ExpenseChart;
